5c8dafcb4ee90db2c44bc6557ae6a8a784a14400
Ruthlesx <ifeoramicheal@gmail.com>
2025-01-19 10:10:45 +0100

    Latest commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # learnable_task4
+
+## TItle of the system:
+    Hotel Management System
+
+## Description:
+    This system allows guests to book and check availabilty of hotel rooms, it manages record of guests,the number and availabilty of rooms
+
+
+
+## Class Diagram Link:
+
+                https://lucid.app/lucidchart/5c7729f5-185f-46fc-88a4-4c93914a4724/edit?viewport_loc=-1390%2C602%2C3665%2C1674%2CHWEp-vi-RSFO&invitationId=inv_0a63eb4c-0dfe-4032-af33-64712043bc2c
--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-// Title of the system: Hotel Mangement system
-//Description: This system allows guests to book and check availabilty of hotel rooms,
-//             it manages record of guests, the number and availabilty of rooms 
-// and also allows hotel staff to manage the rooms and guests.
+
 
 
 class Hotel {
@@ -9,6 +6,7 @@ class Hotel {
     //Static propertiec
     static totalRooms = 50;
     static availableRooms = 42;
+    static rooms = []
 
     constructor(name, location, rooms) {
         this.name = name;
@@ -16,8 +14,11 @@ class Hotel {
 
     }
 
-    addRoom() {
-
+    addRoom(room) {
+      Hotel.rooms.push(room);
+      Hotel.totalRooms++;
+      if (room.isAvailable) Hotel.availableRooms++;
+      console.log(`Room ${room.roomNumber} has been added.`)
     }
 
     viewRooms() {
@@ -28,6 +29,10 @@ class Hotel {
 
 
 class Guest {
+
+    static totalGuests = 0;
+
+
     constructor (name, email, contact) {
         this.name = name;
         this.email = email;
@@ -51,11 +56,15 @@ class Guest {
 //Room class 
 
 class Room {
+
+    static roomCount = 0;
+
     constructor (roomNumber, roomType, pricePerNight, isAvailable = true) {
         this.roomNumber = roomNumber;
         this.roomType = roomType;
         this.pricePerNight = pricePerNight;
         this.isAvailable = isAvailable;
+        Room.roomCount++;
     }
 
     checkAvailability() {
@@ -74,18 +83,23 @@ class Room {
 //Booking class
 
 class Booking {
+
+    static bookingCount = 0;
+
     constructor (guest, room, checkInDate, checkOutDate, status = "Confirmed" ) {
         this.guest = guest;
         this.room = room;
         this.checkInDate = checkInDate;
         this.checkOutDate = checkOutDate;
         this.status = status;
+        Booking.bookingCount++
 
     }
 
     makeBooking() {
         if(this.room.checkAvailability()) {
             this.room.isAvailable = false;
+            Hotel.availableRooms--;
             console.log(`Booking confirmed for ${this.guest.name}.`)
         } else {
             console.log("Room is not available.")
@@ -95,6 +109,7 @@ class Booking {
     cancelBooking() {
         this.status = 'Cancelled';
         this.room.isAvailable = true;
+        Hotel.availableRooms++;
         console.log("Booking has been cancelled.")
     }
 
@@ -109,11 +124,15 @@ class Booking {
 
     //Payment class 
     class Payment {
+
+        static paymentHistory = []
+
         constructor(paymentId, amount, paymentDate, status) {
             this.paymentId = paymentId;
             this.amount = amount;
             this.paymentDate = paymentDate;
             this.status = status;
+            Payment.paymentHistory.push(this)
             
         }
 
@@ -129,21 +148,29 @@ class Booking {
 
 
     class Admin {
+
+        static totalAdmins = 0;
+
         constructor(name, email, contact) {
             this.name = name;
             this.email = email;
             this.contact = contact;
+            Admin.totalAdmins++;
         }
 
-        addRoom(rooms, room) {
-            rooms.push(room);
+        addRoom(room) {
+            Hotel.rooms.push(room);
+            Hotel.totalRooms++;
+            if (room.isAvailable) Hotel.availableRooms++
             console.log(`Room ${rooms.roomNumber} has been added. `)
         }
 
-        removeRoom(rooms, roomNumber) {
-            const index = rooms.findIndex(room => room.roomNumber  === roomNumber);
+        removeRoom(roomNumber) {
+            const index = Hotel.rooms.findIndex((room) => room.roomNumber  === roomNumber);
             if (index !== -1) {
-                rooms.splice(index, 1)
+                const [removedRoom] = Hotel.rooms.splice(index, 1)
+                Hotel.totalRooms--;
+                if (removedRoom.isAvailable) Hotel.availableRooms--;
                 console.log(`Room with number ${roomNumber} has been removed.`)
             } else {
                 console.log("Room not found")
@@ -159,7 +186,9 @@ class Booking {
 
     //Create Guest
     const guest1 = new Guest('John Shaft', "johnshaft504@gmail.com", 2349139856734 )
+    const guest2 = new Guest('Jane Doe', "janedoe678@gmail.com", 8529376412345 )
     guest1.register()
+    guest2.register()
 
     console.log(guest1)
 
@@ -167,13 +196,15 @@ class Booking {
     //Create Rooms
     const room1 = new Room("101", "Single", "$55",true )
     const room2 = new Room("102", "Deluxe Suite", "$250", false)
-    room2.updateDetails()
+    room2.updateDetails("suite", "$200")
    
     
 
     // Create Booking
     const booking1 = new Booking( guest1, room1, "2025-01-20", "2025-01-25");
+    const booking2 = new Booking(guest2, room2, "2025-01-10", "2025-01-15");
     booking1.makeBooking();
+    booking2.makeBooking();
 
     //Process Payment
     const payment1 = new Payment(1, 500, "2025-01-15",);
